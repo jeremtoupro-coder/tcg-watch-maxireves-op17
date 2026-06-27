eb4b632c186ec9ecdb96b33fe8127365d6582c58
jeremtoupro-coder <jeremie.touitou.pro@gmail.com>
2026-06-27 16:33:32 +0200

    refactor(connectors): centralize store registry

---
+++ cloudflare-worker/src/connectors/index.ts
@@ -0,0 +1,27 @@
+import { fantasySphere } from "./fantasySphere";
+import { ludotrotter } from "./ludotrotter";
+import { maxireves } from "./maxireves";
+import { oupi } from "./oupi";
+import type { ConnectorDefinition, StoreKey } from "../types";
+
+export const CONNECTORS: ConnectorDefinition[] = [
+  maxireves,
+  ludotrotter,
+  oupi,
+  fantasySphere
+];
+
+export const DEFAULT_CLOUDFLARE_STORES: StoreKey[] = [
+  "maxireves",
+  "ludotrotter",
+  "oupi"
+];
+
+export function selectConnectors(storeKeys: StoreKey[]): ConnectorDefinition[] {
+  const requested = new Set(storeKeys);
+  return CONNECTORS.filter((connector) => requested.has(connector.key));
+}
+
+export function isStoreKey(value: string): value is StoreKey {
+  return CONNECTORS.some((connector) => connector.key === value);
+}
