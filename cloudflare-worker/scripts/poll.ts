890f107c80bd75c7092897e6232d6dbed6ae3a03
jeremtoupro-coder <jeremie.touitou.pro@gmail.com>
2026-06-28 02:44:50 +0200

    feat(poll): add store audit imports

--- cloudflare-worker/scripts/poll.ts
+++ cloudflare-worker/scripts/poll.ts
@@ -1,1 +1,5 @@
+import { auditConnector } from "../src/audit";
+import { createConfiguredConnector } from "../src/connectorBuilder";
+import { getEnabledStoreDefinitions } from "../src/storeConfig";
+
 export const POLL_VERSION = 1;
