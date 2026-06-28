f1971640f638044be850bc4182dfef199b7e22c1
jeremtoupro-coder <jeremie.touitou.pro@gmail.com>
2026-06-28 02:32:50 +0200

    feat(discovery): connect builder dependencies

--- cloudflare-worker/src/connectorBuilder.ts
+++ cloudflare-worker/src/connectorBuilder.ts
@@ -1,1 +1,5 @@
+import { staticSources, sitemapSourcesForStore } from "./discovery";
+import { getEnabledStoreDefinitions } from "./storeConfig";
+import type { ConnectorDefinition } from "./types";
+
 export const CONNECTOR_BUILDER_VERSION = 1;
