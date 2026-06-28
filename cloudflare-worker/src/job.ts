5f6d5df011ba66c0805bb6e7068c91675519f2d1
jeremtoupro-coder <jeremie.touitou.pro@gmail.com>
2026-06-28 03:28:33 +0200

    chore: remove abandoned job prototype

--- cloudflare-worker/src/job.ts
+++
@@ -1,5 +0,0 @@
-import { auditConnector } from "./audit";
-import { createConfiguredConnector } from "./connectorBuilder";
-import { getEnabledStoreDefinitions } from "./storeConfig";
-
-export const JOB_VERSION = 1;
