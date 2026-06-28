9e19ea3ea59945768cd4df2fe98a8c361216d02e
jeremtoupro-coder <jeremie.touitou.pro@gmail.com>
2026-06-28 02:53:08 +0200

    feat(poll): import product processor

--- cloudflare-worker/scripts/poll.ts
+++ cloudflare-worker/scripts/poll.ts
@@ -1,5 +1,6 @@
 import { auditConnector } from "../src/audit";
 import { createConfiguredConnector } from "../src/connectorBuilder";
+import { processProducts } from "../src/engine";
 import { getEnabledStoreDefinitions } from "../src/storeConfig";
 
 export const POLL_VERSION = 1;
