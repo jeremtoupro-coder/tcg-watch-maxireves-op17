c2b06cf7d0b38e001c205f6b5825efa227bd3e67
jeremtoupro-coder <jeremie.touitou.pro@gmail.com>
2026-06-28 02:54:45 +0200

    feat(poll): import persistence facade

--- cloudflare-worker/scripts/poll.ts
+++ cloudflare-worker/scripts/poll.ts
@@ -1,6 +1,7 @@
 import { auditConnector } from "../src/audit";
 import { createConfiguredConnector } from "../src/connectorBuilder";
 import { processProducts } from "../src/engine";
+import { createRemoteState } from "../src/persistence";
 import { getEnabledStoreDefinitions } from "../src/storeConfig";
 
 export const POLL_VERSION = 1;
